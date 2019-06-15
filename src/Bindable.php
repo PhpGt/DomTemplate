c49573e7c66c2868f2ba8951f6f93f02e92a5df4
Greg Bowler <greg.bowler@g105b.com>
2019-06-15 23:12:02 +0100

    Delete old implementation

--- src/Bindable.php
+++ src/Bindable.php
@@ -3,8 +3,6 @@ namespace Gt\DomTemplate;
 
 use Gt\Dom\Attr as BaseAttr;
 use Gt\Dom\Element as BaseElement;
-use DOMNode;
-use StdClass;
 use Gt\Dom\HTMLCollection as BaseHTMLCollection;
 
 /**
@@ -51,7 +49,6 @@ trait Bindable {
 	 * will have the inner iterable data bound to it before being added into
 	 * the DOM in the position that it was originally extracted.
 	 *
-	 * TODO: Enforce the following:
 	 * When $templateName is not provided, the data within $kvpList will be
 	 * bound to an element that has a data-template attribute with no value.
 	 * If there are multiple un-named template elements, an exception is
@@ -86,12 +83,11 @@ trait Bindable {
 	}
 
 	/**
-	 * When data needs binding to a nested DOM structure, a BindIterator is
-	 * necessary to link each child list with the correct template.
-	 *
-	 * TODO: Implement.
+	 * When complex data needs binding to a nested DOM structure, a
+	 * BindIterator is necessary to link each child list with the
+	 * correct template.
 	 */
-	public function bindNestedList(BindIterator $iterator):void {
+	public function bindNestedList(iterable $data):void {
 
 	}
 
@@ -239,195 +235,6 @@ trait Bindable {
 		}
 	}
 
-	protected function bindTemplates(
-		DOMNode $element,
-		$data,
-		string $templateName = null
-	):void {
-		if($element instanceof \DOMDocumentFragment) {
-			return;
-		}
-
-		$namesToMatch = [];
-
-		if(is_null($templateName)) {
-			$namesToMatch []= $element->getNodePath();
-
-		}
-		else {
-			$namesToMatch []= $templateName;
-		}
-
-		/** @var HTMLDocument $rootDocument */
-		$rootDocument = $this->getRootDocument();
-		/** @var DocumentFragment[] $templateChildren */
-		$templateChildren = $rootDocument->getNamedTemplateChildren(
-			...$namesToMatch
-		);
-
-		foreach($data as $rowIndex => $row) {
-			foreach($templateChildren as $childNumber => $fragment) {
-				$insertInto = null;
-
-				if($fragment->templateParentNode !== $element) {
-					$insertInto = $element;
-				}
-
-				$newNode = $fragment->insertTemplate($insertInto);
-				$this->injectDataIntoBindProperties($newNode, $row);
-				$this->injectDataIntoAttributeValues(
-					$newNode,
-					$row
-				);
-			}
-		}
-
-		if(is_null($rowIndex)) {
-			$trimmed = trim($element->innerHTML);
-			if($trimmed === "") {
-				$element->innerHTML = "";
-			}
-		}
-	}
-
-	protected function setData(BaseElement $element, $data):void {
-		if(is_array($data)) {
-			$data = $this->convertArrayToObject($data);
-		}
-
-		foreach($element->attributes as $attr) {
-			$matches = [];
-			if(!preg_match(
-				"/(?:data-bind:)(.+)/",
-				$attr->name,
-				$matches)
-			) {
-				continue;
-			}
-			$bindProperty = $matches[1];
-
-			if($bindProperty === "class") {
-				$this->handleClassData(
-					$attr,
-					$element,
-					$data
-				);
-			}
-			else {
-				$this->handlePropertyData(
-					$attr,
-					$bindProperty,
-					$element,
-					$data
-				);
-			}
-		}
-	}
-
-	protected function handlePropertyData(
-		BaseAttr $attr,
-		string $bindProperty,
-		BaseElement $element,
-		$data
-	):void {
-		$dataKeyMatch = $this->getKeyFromAttribute($element, $attr);
-		$dataValue = $dataKeyMatch->getValue($data) ?? "";
-
-		switch($bindProperty) {
-		case "html":
-		case "innerhtml":
-			$element->innerHTML = $dataValue;
-			break;
-
-		case "text":
-		case "innertext":
-		case "textcontent":
-			$element->innerText = $dataValue;
-			break;
-
-		case "value":
-			$element->value = $dataValue;
-			break;
-
-		default:
-			$element->setAttribute($bindProperty, $dataValue);
-			break;
-		}
-	}
-
-	protected function handleClassData(
-		BaseAttr $attr,
-		BaseElement $element,
-		$data
-	):void {
-		$classList = explode(" ", $attr->value);
-		$this->setClassFromData(
-			$element,
-			$data, ...
-			$classList
-		);
-	}
-
-	protected function setClassFromData(
-		BaseElement $element,
-		$data,
-		string...$classList
-	):void {
-		foreach($classList as $class) {
-			if(!strstr($class, ":")) {
-				$class = "$class:$class";
-			}
-
-			list($keyMatch, $className) = explode(":", $class);
-
-			if(!isset($data->{$keyMatch})) {
-				continue;
-			}
-
-			if($data->{$keyMatch}) {
-				$element->classList->add($className);
-			}
-			else {
-				$element->classList->remove($className);
-			}
-		}
-	}
-
-	protected function getKeyFromAttribute(BaseElement $element, Attr $attr):DataKeyMatch {
-		$required = true;
-		$key = $attr->value;
-
-		if($key[0] === "?") {
-			$required = false;
-			$key = substr($key, 1);
-		}
-
-		if($key[0] === "@") {
-			$key = substr($key, 1);
-			$attributeValue = $element->getAttribute($key);
-			if(is_null($attributeValue)) {
-				throw new BoundAttributeDoesNotExistException($attr->name);
-			}
-
-			$key = $attributeValue;
-		}
-
-		return new DataKeyMatch($key, $required);
-	}
-
-	protected function getTemplateNamesForElement(BaseElement $element):array {
-		$templateNames = [];
-		$nodePath = $element->getNodePath();
-
-		foreach($this->templateFragmentMap as $key => $templateFragment) {
-			if(strpos($key, $nodePath) === 0) {
-				$templateNames []= $key;
-			}
-		}
-
-		return $templateNames;
-	}
-
 	protected function getChildrenWithBindAttribute():BaseHTMLCollection {
 		/** @var BaseElement $element */
 		$element = $this;
@@ -438,39 +245,5 @@ trait Bindable {
 		return $element->xPath(
 			"descendant-or-self::*[@*[starts-with(name(), 'data-bind')]]"
 		);
-	}
-
-	protected function cleanBindAttributes(DOMNode $element):void {
-		$elementsToClean = [$element];
-		$childrenWithBindAttribute = $this->getChildrenWithBindAttribute($element);
-		foreach($childrenWithBindAttribute as $child) {
-			$elementsToClean []= $child;
-		}
-
-		foreach($elementsToClean as $cleanMe) {
-			if(!$cleanMe->attributes) {
-				continue;
-			}
-
-			$attributesToRemove = [];
-			foreach($cleanMe->attributes as $attr) {
-				if(strpos($attr->name, "data-bind") === 0) {
-					$attributesToRemove []= $attr->name;
-				}
-			}
-
-			foreach($attributesToRemove as $attrName) {
-				$cleanMe->removeAttribute($attrName);
-			}
-		}
-	}
-
-	protected function convertArrayToObject(array $array) {
-		$object = new StdClass();
-		foreach($array as $key => $value) {
-			$object->$key = $value;
-		}
-
-		return $object;
 	}
 }
