08fc5f43459aa2b864af67974ea1903665a818c7
Greg Bowler <greg.bowler@g105b.com>
2019-12-01 21:46:39 +0000

    Bind true/false values, closes #116

--- test/unit/Helper/BindDataGetter/TodoItem.php
+++ test/unit/Helper/BindDataGetter/TodoItem.php
@@ -6,13 +6,16 @@ use Gt\DomTemplate\BindObject;
 class TodoItem implements BindObject {
 	private $id;
 	private $title;
+	private $completed;
 
 	public function __construct(
 		int $id,
-		string $title
+		string $title,
+		bool $completed = false
 	) {
 		$this->id = $id;
 		$this->title = $title;
+		$this->completed = $completed;
 	}
 
 	public function getTitle():string {
@@ -23,7 +26,12 @@ class TodoItem implements BindObject {
 	public function bindId():int {
 		return $this->id;
 	}
+
 	public function bindTitle():string {
 		return $this->getTitle();
+	}
+
+	public function bindCompleted():bool {
+		return $this->completed;
 	}
 }
