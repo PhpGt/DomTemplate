65a5e13932e1b44fc7366175c7198142f558bf90
Greg Bowler <greg.bowler@g105b.com>
2022-01-30 14:38:41 +0000

    feature: implement select value binding closes #317

--- src/HTMLAttributeBinder.php
+++ src/HTMLAttributeBinder.php
@@ -5,6 +5,7 @@ use Gt\Dom\Document;
 use Gt\Dom\DOMTokenList;
 use Gt\Dom\Element;
 use Gt\Dom\Facade\DOMTokenListFactory;
+use Gt\Dom\HTMLElement\HTMLSelectElement;
 
 class HTMLAttributeBinder {
 	private TableBinder $tableBinder;
@@ -181,7 +182,21 @@ class HTMLAttributeBinder {
 				);
 			}
 			else {
-				$element->setAttribute($bindProperty, $bindValue);
+				if($element instanceof HTMLSelectElement
+				&& $bindProperty === "value") {
+					foreach($element->options as $option) {
+						$optionValue = $option->value;
+						if($bindValue == $optionValue) {
+							$option->selected = true;
+						}
+						else {
+							$option->selected = false;
+						}
+					}
+				}
+				else {
+					$element->setAttribute($bindProperty, $bindValue);
+				}
 			}
 
 			break;
